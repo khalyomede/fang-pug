c2622c5a8f3a6a353087bf6e403dd4e50aa5b3a0
khalyomede <>
2019-03-10 00:08:40 +0100

    removed debug mode

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,5 @@
 import * as pug from "pug";
 import * as rename from "rename";
-import * as cliColor from "cli-color";
-import * as fancyLog from "fancy-log";
 
 const fangPug = options => fang => {
 	fang.files.map(file => {
@@ -15,10 +13,6 @@ const fangPug = options => fang => {
 		file.path = rename(file.path, {
 			extname: ".html"
 		});
-
-		if (fang.options.debug) {
-			fancyLog.info(`${cliColor.magenta(file.path)}: turned into html`);
-		}
 
 		return file;
 	});
