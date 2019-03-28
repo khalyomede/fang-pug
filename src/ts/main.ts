a91381eb3e11cb95ca60b687c1806e4fe33ee534
khalyomede <>
2019-03-28 23:06:06 +0100

    added support for debug mode

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,17 @@
 import * as pug from "pug";
 import * as rename from "rename";
+import { basename } from "path";
 
 const fangPug = options => fang => {
+	fang.pluginName = "fang-pug";
+
 	fang.files.map(file => {
+		const fileName = basename(file.path);
+
+		if (fang.options.debug) {
+			fang.info(`rendering ${fileName}...`);
+		}
+
 		file.content = Buffer.from(
 			pug.render(file.content.toString(), {
 				filename: file.path,
@@ -13,6 +22,10 @@ const fangPug = options => fang => {
 		file.path = rename(file.path, {
 			extname: ".html"
 		});
+
+		if (fang.options.debug) {
+			fang.info(`rendered ${fileName}`);
+		}
 
 		return file;
 	});
